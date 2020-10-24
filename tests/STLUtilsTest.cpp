96935ae59459e0daa72c5c15629cf20630ca12ad
Defu <gudo35@gmail.com>
2020-10-24 14:02:58 +0200

    Implemented some STLUtils tests

--- tests/STLUtilsTest.cpp
+++ tests/STLUtilsTest.cpp
@@ -34,4 +34,42 @@ TEST_CASE("Vector test")
         CHECK(VectorUtils::IsInside(vec, 6) == true);
         CHECK(VectorUtils::IsInside(vec, 8) == true);
     }
+    SUBCASE("Union")
+    {
+        std::vector<int> b{1, 4, 5, 6, 7};
+        auto res = VectorUtils::Union(vec, b);
+
+        CHECK(VectorUtils::IsInside(res, 1) == true);
+        CHECK(VectorUtils::IsInside(res, 2) == true);
+        CHECK(VectorUtils::IsInside(res, 3) == true);
+        CHECK(VectorUtils::IsInside(res, 4) == true);
+        CHECK(VectorUtils::IsInside(res, 5) == true);
+        CHECK(VectorUtils::IsInside(res, 6) == true);
+        CHECK(VectorUtils::IsInside(res, 7) == true);
+
+        CHECK(VectorUtils::IsInside(res, 8) == false);
+        CHECK(VectorUtils::IsInside(res, 9) == false);
+    }
+}
+
+TEST_CASE("String test")
+{
+    std::string string = "A quick fox jumps over a rock\n";
+
+    SUBCASE("Replace")
+    {
+        std::string tokenA = "fox";
+        std::string replacerA = "toad";
+        const std::string expectedResultA = "A quick toad jumps over a rock\n";
+
+        std::string resA = StringUtils::Replace(string, tokenA, replacerA);
+        CHECK(resA == expectedResultA);
+
+        std::string tokenB = "rock";
+        std::string replacerB = "tree";
+        const std::string expectedResultB = "A quick fox jumps over a tree\n";
+
+        std::string resB = StringUtils::Replace(string, tokenB, replacerB);
+        CHECK(resB == expectedResultB);
+    }
 }
