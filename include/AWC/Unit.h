f73123e5ccbe8cb1e3f20929225dd4caa7466766
Defu <gudo35@gmail.com>
2020-11-04 18:31:54 +0100

    Implements Unit attack interface
    Needs testing

--- include/AWC/Unit.h
+++ include/AWC/Unit.h
@@ -17,13 +17,43 @@ public:
     const std::string GetName() const;
     const uint GetId() const;
 
+    // Misc
+    // TODO: Future release. Needs WeaponType and MovementType interface implementation.
+    // UnitType GetUnitType(); 
+
     // Movement
     UnitMovement CalculateMovement(const Map& map, Vector2 origin);
+    uint Move(unsigned int moveCost);
 
     // Attack
+
+    UnitAttack CalculateAttack(unsigned int weaponId, const Map& map, Vector2 origin);
+
+    // Can this unit attack given unit? Takes currentAmmo into account
     bool CanAttack(UnitPtr unit);
-    bool CanAttackWith(UnitPtr unit, uint weaponId);
+
+    // Can this unit attack a give unit with a specific? Takes current ammo into account
+    bool CanAttackUnitWith(UnitPtr unit, uint weaponId);
+
+    // How many weapons this unit has?
     uint GetWeaponCount();
+
+    // How much damage the weapon of this unit deals to a given unit. Takes into account dmg mods but ignores defense mods.
+    float GetDmgToUnit(unsigned int weaponId, UnitPtr unit);
+
+    // Uses weapon with given id, consuming ammo.
+    void UseWeapon(unsigned int weaponId);
+
+    // Defense
+
+    // How much damage takes this unit after defense mods?
+    float GetDmgTaken(float incomingDmg);
+
+    // Reduces this unit's health by a raw amount. Ignores defense mods.
+    void TakeRawDamage(float incomingDmg);
+
+    // Reduces this unit's health by a given amount. Takes defense mods into account.
+    void TakeDamge(float incomingRawDmg);
 
 private:
 
@@ -31,7 +61,10 @@ private:
     TilePatternConstraints GetMoveConstraints() const;
 
     // Attack
+    TilePatternConstraints GetAttackConstraints(unsigned int weaponId) const;
     bool IsWeaponIdValid(uint weaponId);
+
+    void ThrowInvalidWeaponIdException(uint weaponId);
 
     const UnitType& unitType_;
     const MovementDescPtr moveDesc_;
--- src/AWC/Unit.cpp
+++ src/AWC/Unit.cpp
@@ -1,6 +1,7 @@
 #include <AWC/Unit.h>
 #include <AWC/UnitType.h>
 #include <AWC/UnitMovement.h>
+#include <AWC/UnitAttack.h>
 
 #include <AWC/TilePatternConstraints.h>
 
@@ -9,6 +10,8 @@
 #include <AWC/Weapon.h>
 #include <AWC/WeaponType.h>
 #include <AWC/TilePatternDesc.h>
+#include <AWC/TilePattern.h>
+#include <AWC/CostTable.h>
 
 #include <AWC/AWCException.h>
 
@@ -39,25 +42,42 @@ UnitMovement Unit::CalculateMovement(const Map& map, Vector2 origin)
 
 // Attack
 
+UnitAttack Unit::CalculateAttack(unsigned int weaponId, const Map& map, Vector2 origin)
+{
+    UnitAttack unitAttack{TilePatternIPtr{nullptr}};
+    if(IsWeaponIdValid(weaponId))
+    {
+        auto weapon = weapons_[weaponId];
+        auto tpd = weapon->GetAttackPattern();
+        auto attackConstraints = GetAttackConstraints(weaponId);
+        auto tp = tpd->CalculateTilePattern(map, origin, attackConstraints);
+        unitAttack = UnitAttack{tp};
+    }
+    else
+        ThrowInvalidWeaponIdException(weaponId);
+    
+    return unitAttack;
+}
+
 bool Unit::CanAttack(UnitPtr unit)
 {
     for(uint weaponId = 0; weaponId < GetWeaponCount(); weaponId++)
-        if(CanAttackWith(unit, weaponId))
+        if(CanAttackUnitWith(unit, weaponId))
             return true;
     
     return false;
 }
 
-bool Unit::CanAttackWith(UnitPtr unit, uint weaponId)
+bool Unit::CanAttackUnitWith(UnitPtr unit, uint weaponId)
 {
     bool canAttack = false;
     if(IsWeaponIdValid(weaponId))
     {
         auto weapon = weapons_[weaponId];
-        canAttack = weapon->CanAttackUnit(unit->GetId());
+        canAttack = weapon->HasEnoughAmmo() && weapon->CanAttackUnit(unit->GetId());
     }
     else    
-        throw std::out_of_range{"Invalid weaponId: " + std::to_string(weaponId)};
+        ThrowInvalidWeaponIdException(weaponId);
 
     return canAttack;
 }
@@ -65,6 +85,52 @@ bool Unit::CanAttackWith(UnitPtr unit, uint weaponId)
 uint Unit::GetWeaponCount()
 {
     return weapons_.size();
+}
+
+float Unit::GetDmgToUnit(unsigned int weaponId, UnitPtr unit)
+{
+    // This can be modified by dmg buffs
+    float dmg = 0.0f;
+    if(IsWeaponIdValid(weaponId))
+    {
+        auto weapon = weapons_[weaponId];
+        dmg = weapon->GetBaseDamageTo(unit->GetId());
+    }
+    else
+        ThrowInvalidWeaponIdException(weaponId);
+
+    return dmg;
+}
+
+
+void Unit::UseWeapon(unsigned int weaponId)
+{
+    if(IsWeaponIdValid(weaponId))
+    {
+        auto weapon = weapons_[weaponId];
+        weapon->Use();
+    }
+    else
+        ThrowInvalidWeaponIdException(weaponId);
+}
+
+// Defense
+
+float Unit::GetDmgTaken(float incomingDmg)
+{
+    // This can by modified by buffs
+    return incomingDmg;
+}
+
+void Unit::TakeRawDamage(float incomingDmg)
+{
+    // Reduces health by exact amount. Ignoring buffs
+}
+
+void Unit::TakeDamge(float incomingRawDmg)
+{
+    // Uses defense mods to calculate final health reduction. C
+    GetDmgTaken(incomingRawDmg);
 }
 
 // private
@@ -84,7 +150,24 @@ TilePatternConstraints Unit::GetMoveConstraints() const
 
     // Attack
 
+TilePatternConstraints Unit::GetAttackConstraints(unsigned int weaponId) const
+{
+    auto weapon = weapons_[weaponId];
+
+    CostTableIPtr fixedCost{ new FixedCostTable{1} };
+    CostTableIPtr unitFixedCost{ new FixedCostTable{0} };
+
+    // Range could be affected by mods;
+
+    TilePatternConstraints{fixedCost, unitFixedCost, weapon->GetAttackRange()};
+}
+
 bool Unit::IsWeaponIdValid(uint weaponId)
 {
     return weaponId < weapons_.size();
+}
+
+void Unit::ThrowInvalidWeaponIdException(uint weaponId)
+{
+    throw std::out_of_range{"Invalid weaponId: " + std::to_string(weaponId)};
 }
