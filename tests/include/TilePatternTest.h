1c1a894657da41b066b8180d255eb5454dd5fcd8
martingonzalezh <martingonzalezh@alumnos.uvigo.es>
2020-12-07 13:54:15 +0100

    Removed undefined functions in TilePatternTest.h

--- tests/include/TilePatternTest.h
+++ tests/include/TilePatternTest.h
@@ -5,7 +5,4 @@
 namespace TilePatternTest
 {
     std::vector<Vector2> GetUnreachableTiles(const Map& map, const Directions& reachableTiles);
-
-    TilePatternDescIPtr GetRookDesc();
-    TilePatternDescIPtr GetBishopDesc();
 };
