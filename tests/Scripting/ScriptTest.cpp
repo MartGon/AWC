d107c4be15c9016ea2e6a0279c7ce576e6f1db5b
Defu <gudo35@gmail.com>
2021-02-15 19:25:02 +0100

    Added small lua test

--- tests/Scripting/ScriptTest.cpp
+++ tests/Scripting/ScriptTest.cpp
@@ -1,7 +1,28 @@
 #include "doctest.h"
 
+#include <lua.hpp>
+
+#include <string>
 
 TEST_CASE("Script test")
 {
-    CHECK(true == true);
+    bool alright = false;
+    auto luaState = luaL_newstate();
+
+    std::string testFile = std::string(SCRIPTS_DIR) + "test.lua";
+    auto res = luaL_loadfile(luaState, testFile.c_str());
+
+    if(res == LUA_OK)
+    {
+        if(LUA_OK == lua_pcall(luaState, 0, 0, 0))
+        {
+            auto type = lua_getglobal(luaState, "TEST");
+            int value = lua_tointeger(luaState, -1);
+            alright = value == 3;
+        }
+    }
+
+    lua_close(luaState);
+
+    CHECK(alright == true);
 }
