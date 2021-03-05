4e30a5b0fddc25e86a27e8d44f00c30c0a965b90
Defu <gudo35@gmail.com>
2021-03-05 16:56:24 +0100

    Tests UnitMovement UserData

---
+++ resources/tests/Scripts/UserData/UnitMovement/CanMove.lua
@@ -0,0 +1,13 @@
+
+
+function Execute(game)
+
+    if unitMove:CanMove(dest) then
+        canMove = true;
+    end
+
+end
+
+function Undo(game)
+
+end
---
+++ tests/Script/src/UserData/UnitMovement.cpp
@@ -0,0 +1,55 @@
+#include "doctest.h"
+
+#include <AWC.h>
+#include <Script.h>
+
+#include <Test/Script/Script.h>
+#include <Tests/AWC/UnitTest.h>
+
+#include <iostream>
+
+TEST_CASE("Unit userdata")
+{
+    Script::Game sGame;
+    auto& game = sGame.GetGame();
+
+    Map map{5, 5};
+    TileType grassType{0, "Grass"};
+    MapUtils::FillMap(map, grassType);
+
+    Player p{0, 0, 1000};
+    game.AddPlayer(p);
+
+    auto soldierType = UnitTest::CreateSoldierType();
+    auto soldier = soldierType.CreateUnit(p);
+
+    game.AddMap(map);
+
+    game.AddUnit(soldier, {2, 0}, 0);
+    
+    SUBCASE("CalculateMovement")
+    {
+        std::string path = Test::Script::GetUserDataPath() + "/UnitMovement/CanMove.lua";
+        Test::Script::TestScript t(path, sGame);
+        
+        auto L = sGame.GetLuaState();
+
+        auto& sTable = t.lt();
+        auto unitMove = soldier->CalculateMovement(game.GetMap(0), {2, 0});
+        
+        sTable.SetFullUserData("unitMove", Script::UserData::UnitMovement::MT_NAME, unitMove);
+        sTable.SetFullUserData("dest", Script::UserData::Vector2::MT_NAME, Vector2{0, 0});
+
+        try{        
+            sGame.PushScript(t.ref);
+            game.Run();
+        }
+        catch(const std::bad_alloc& e)
+        {
+            std::cout << e.what() << '\n';
+        }
+
+        auto canMove = sTable.GetBool("canMove");
+        CHECK(canMove == true);
+    }
+}
